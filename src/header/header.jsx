78766837ad6feba1e70f128b682c13a4194b4c26
Alexey Ovchinnikov <gdhald4@gmail.com>
2024-03-22 15:25:43 +0500

    finished create header module

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -7,15 +7,29 @@ class Header extends React.Component {
         return (
             <>
                 <header>
-                    <img src="/header_img/Логотип.svg" alt="" className='header_logo' />
+                    <img src="src\header\header_img\Логотип.svg" alt="" className='header_logo' />
                     <div className="center_right">
                         <div className="center_txt">
-                            <img src="/header_img/location.svg" alt="" />
+                            <img src="src\header\header_img\location.svg" alt="" />
                             <p>ОФИС: СПб, Пулковское шоссе, дом 30 к 4, офис 205 <br />
                                 Производство: СПб, Пулковское шоссе, дом 30 к 4</p>
                         </div>
                         <div className="right_obj">
+                            <div className="first_item">
+                                <img src="src\header\header_img\Phone_call.svg" alt="" />
+                                <p>
+                                    круглосуточно
+                                    <br /> +7 (000) 000-00-00</p>
+                            </div>
+                            <div className="second_item">
+                                <img src="src\header\header_img\Globe 2.svg" alt="" />
+                                <p>
+                                    САЙТ КОМПАНИИ
+                                    <br /> WWW.NEWTECK.RU
+                                </p>
+                            </div>
                         </div>
+
                     </div>
 
                 </header>
