1eaa2fdc0acf8e43d76c4c7f800860a10d742c8b
Alexey Ovchinnikov <gdhald4@gmail.com>
2024-03-22 14:38:00 +0500

    Rename Main to APP

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
-import Header from './header/header'
+import Header from '../header/header'
+import './app.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
